fc269d9dfefc91670cc637c24735d9898ab5cc99
JCrossover-14 <110313504+JCrossover-14@users.noreply.github.com>
2026-01-20 00:16:41 -0500

    updated requirements, added model draft

--- chessenv.py
+++ chessenv.py
@@ -1,5 +1,6 @@
 
 import chess
+import numpy as np
 
 class ChessEnv:
     def __init__(self):
@@ -7,7 +8,7 @@ class ChessEnv:
     
     def copy(self):
         env = ChessEnv()
-        env.board = self.board.copy(stack=False)
+        env.board = self.board.copy(stack = True)
         return env
     def legal_moves(self):
         return list(self.board.legal_moves)
@@ -30,3 +31,74 @@ class ChessEnv:
         # in python chess true refers to white false refers to black
         return 1 if outcome.winner else -1
 
+    # encodes current position from perspective of current player
+    def encode(self):
+        '''
+        AlphaZero style encoding
+
+        Returns:
+            planes: np.ndarray of shape (21, 8 ,8)
+        
+        :param self: Description
+        '''
+        
+        planes = np.zeros((21,8,8), dtype = np.float32)
+
+        # Piece planes 
+        # Planes 0-5: p1 pieces
+        # Planes 6-11: p2 pieces
+        # Piece type enums directly correspond to plane
+        for square, piece in self.board.piece_map().items():
+            row = 7-chess.square_rank(square)
+            col = chess.square_file(square)
+
+            if piece.color == self.board.turn:
+                planes[piece.piece_type-1, row, col]=1.0
+            else:
+                planes[piece.piece_type+5,row,col]=1.0
+
+        # repetition planes 12-13
+        # plane 12 represents repeated once 
+        # plane 13 represents repeated twice
+
+        if self.board.is_repetition(1):
+            planes[12, :, :] = 1.0
+        if self.board.is_repetition(2):
+            planes[13, :, :] = 1.0
+
+        
+        # plane 14: all 1s if its white
+        if self.board.turn:
+            planes[14,:,:]=1.0
+        
+        # plane 15: move count normalized by /100
+        planes[15,:,:] = min(1,self.board.fullmove_number/100)
+
+        # plane 16-17: current player has castling rights
+        if self.board.has_kingside_castling_rights(self.board.turn):
+            planes[16,:,:]=1.0
+        if self.board.has_queenside_castling_rights(self.board.turn):
+            planes[17,:,:]=1.0
+        
+        # plane 18-19: opponent has castling rights
+        opponent = not self.board.turn
+        if self.board.has_kingside_castling_rights(opponent):
+            planes[18,:,:]=1.0
+        if self.board.has_queenside_castling_rights(opponent):
+            planes[19,:,:]=1.0
+
+        # plane 20: halfmove clock normalized by /100
+
+        planes[20,:,:]= min(1,self.board.halfmove_clock/100)
+
+        return planes
+        
+
+        
+
+
+
+            
+            
+
+            
---
+++ model.py
@@ -0,0 +1,97 @@
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F 
+import config 
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels=None, kernel_size=3, padding=1):
+        super().init__()
+        outchannels = out_channels or config.NUM_CHANNELS
+        self.conv = nn.Conv2d(in_channels,out_channels, kernel_size=kernel_size, padding = padding)
+        self.bn = nn.BatchNorm2d(out_channels)
+    
+    def forward(self,x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = F.relu(x)
+        return x 
+
+
+class ResBlock(nn.Module):
+    def __init__(self.channels = None):
+        super().init__()
+        channels = channels or config.NUM_CHANNELS
+        self.conv1 = nn.Conv2d(channels,channels,3,padding=1,bias = False)
+        self.bn1 = nn.BatchNorm2d(channels)
+        self.conv2 = nn.Conv2d(channels,channels,3,padding=1,bias=False)
+        self.bn2=nn.BatchNorm2d(channels)
+    def forward(self,x):
+        residual = x 
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out+=residual 
+        return F.relu(out)
+
+class PolicyHead(nn.Module):
+    def __init__(self,in_channels = None, action_size = None):
+        super().init()
+        in_channels = in_channels or config.NUM_CHANNELS
+        action_size = action_size or config.ACTION_SIZE
+
+        self.conv = nn.Conv2d(in_channels, config.POLICY_HEAD_CHANNELS, kernel_size=1)
+        self.bn = nn.BatchNorm2d(config.POLICY_HEAD_CHANNELS)
+        self.fc = nn.Linear(config.POLICY_HEAD_CHANNELS*8*8, action_size)
+    
+    def forward(self,x,legal_mask = None):
+        x = F.relu(self.bn(self.conv(x)))
+        x = x.view(x.size(0),-1)
+        x = self.fc(x)
+        if legal_mask is not None:
+            x = x.masked_fill(legal_mask==0, float('-inf'))
+        x = F.softmax(x, dim=1)
+        return x 
+
+class ValueHead(nn.Module):
+    def __init__(self,in_channels = None):
+        super().init()
+        in_channels = in_channels or config.NUM_CHANNELS
+
+        self.conv = nn.Conv2d(in_channels, config.VALUE_HEAD_CHANNELS, kernel_size=1)
+        self.bn = nn.BatchNorm2d(config.VALUE_HEAD_CHANNELS)
+        self.fc1 = nn.Linear(config.VALUE_HEAD_CHANNELS*8*8, 256)
+        self.fc2 = nn.Linear(256,1)
+    def forward(self,x):
+        x = F.relu(self.bn(self.conv(x)))
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        return x
+
+class ChessModel(nn.Module):
+    def __init__(self, in_channels = 21, num_res_blocks = None, action_size = None):
+        super().init()
+        num_res_blocks = num_res_blocks or config.NUM_RES_BLOCKS
+        action_size = action_size or config.ACTION_SIZE
+
+        self.conv_block = ConvBlock(in_channels, config.NUM_CHANNELS)
+
+        self.res_blocks = nn.ModuleList(
+            [ResBlock(config.NUM_CHANNELS) for _ in range(num_res_blocks)]
+        )
+
+        self.policy_head = PolicyHead(config.NUM_CHANNELS, action_size)
+        self.value_head = ValueHead(config.NUM_CHANNELS)
+    
+    def forward(self,x, legal_mask = None):
+        x = self.conv_block(x)
+        for res_block in self.res_blocks:
+            x = res_block(x)
+        
+        policy = self.policy_head(x, legal_mask)
+        value = self.value_head(x)
+
+        return policy, value
+
